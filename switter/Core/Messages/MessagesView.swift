34cb2b7e025eac059b48da4cc11e643b29e08168
Sven Wetter <sven.wetter@icloud.com>
2022-11-21 21:30:15 +0100

    Creating explore, notification and message view

---
+++ switter/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  switter
+//
+//  Created by Sven Wetter on 21.11.22.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
---
+++ switter/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  switter
+//
+//  Created by Sven Wetter on 21.11.22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
