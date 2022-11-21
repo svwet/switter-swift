0fb5dd821a72dae75a1c3fe8a192363575c3de02
Sven Wetter <sven.wetter@icloud.com>
2022-11-21 20:50:22 +0100

    Initial Commit

---
+++ switter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  switter
+//
+//  Created by Sven Wetter on 21.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ switter/switterApp.swift
@@ -0,0 +1,17 @@
+//
+//  switterApp.swift
+//  switter
+//
+//  Created by Sven Wetter on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct switterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
