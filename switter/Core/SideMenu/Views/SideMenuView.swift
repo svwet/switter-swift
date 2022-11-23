86181bcbf6e03e328d88cf07fb7a0e811ae37eea
Sven Wetter <sven.wetter@icloud.com>
2022-11-23 21:10:31 +0100

    Handling sidemenuoptionrowview with navigation link

--- switter/Core/SideMenu/Views/SideMenuView.swift
+++ switter/Core/SideMenu/Views/SideMenuView.swift
@@ -29,18 +29,23 @@ struct SideMenuView: View {
             }
             .padding(.leading)
             
-            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { option in
-                HStack(spacing: 16) {
-                    Image(systemName: option.imageName)
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                    Text(option.title)
-                        .font(.subheadline)
-                    
-                    Spacer()
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { viewModel in
+                if viewModel == .profile {
+                    NavigationLink {
+                        ProfileView()
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+                } else if viewModel == .logout {
+                    Button {
+                        print("Handle logout here,,")
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+                } else {
+                    SideMenuOptionRowView(viewModel: viewModel)
                 }
-                .frame(height: 40)
-                .padding(.horizontal)
+                
             }
             
             Spacer()
@@ -53,3 +58,4 @@ struct SideMenuView_Previews: PreviewProvider {
         SideMenuView()
     }
 }
+
