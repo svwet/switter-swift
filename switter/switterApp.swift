9c102249e0a671f87bfcce173bd105ed2f19ca6c
Sven Wetter <sven.wetter@icloud.com>
2022-11-23 20:57:20 +0100

    Link sidemen navigation to the content view

--- switter/switterApp.swift
+++ switter/switterApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct switterApp: App {
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
